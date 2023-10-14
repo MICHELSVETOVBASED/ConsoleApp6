b263013008395360a4acb36cab8fa9ac2a1c4e32
maksk <maksk@KKK>
2023-10-14 12:10:30 +0200

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        string num1 = Console.ReadLine();
+        int Length = num1.Length;
+
+
+
+        string [] sum = new string[; 
+        for ( int i = 0; i<num1.Length; i++ )
+        {
+            for ( int j = i+1; j < num1.Length; j++ )
+            {
+                if (num1[i] == num1[j])
+                {
+                    continue;
+                }
+                else if (num1[i].Equals(sum))
+                {
+                    continue;
+                }
+                else 
+                {
+                    sum += num1[i];
+                    C   
+                }
+            }
+        }
+
+
+           
+
+
+    }
+}
