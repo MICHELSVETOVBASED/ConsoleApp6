8d19f844ea31afa6cf8fb049869c86714b20b1af
Schub Bergman <maks.kon.92@mail.ru>
2023-10-20 13:43:39 +0200

    Tasks with unique element of first string

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,42 +1,42 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
 using System.Linq;
-
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
 
 public class MainClass
 {
     public static void Main()
     {
+        string num = Console.ReadLine();
         string num1 = Console.ReadLine();
-        int[] intnum = new int[num1.Length];
-        for (int i = 0; i < num1.Length; i++)
+        string s = "";
+        
+
+        for ( int i = 0; i<num.Length; i++ )
         {
-            int n = (int)char.GetNumericValue(num1[i]);
-            intnum[i] = n;
-        }
-        int[] newints = new int[intnum.Length];
-        string [] sum = new string[num1.Length];
-
-        for ( int i = 0; i<num1.Length; i++ )
-        {
-            if 
+            int count = 0;
+            for (int j = 0; j < num.Length; j++)
             {
-                continue;
-            }
-            else 
-            {
+                if (num[i] == num[j])
+                {
+                    for (int k = 0; k<num1.Length;k++)
+                    {
+                        if (num[i] == num1[k])
+                            count++;
 
-
-                Console.Write(num1[i]);
-                Console.Write(num1[i]);
-
+                    }
+                    count++;
+                }
 
             }
+            if(count == 1)
+            {
+                s += num[i] + " ";
+            }
+            
         }
-
-
-           
-
-
+        Console.WriteLine(s);
     }
 }
